33b7d110bfff4a77209bec898c2ca6419604f0e8
Felipe <felipe.sampaio@aeb.gov.br>
2016-02-28 00:21:39 -0300

    Criando efeito da navbar quando da o scroll

--- client/template_logic/header.js
+++ client/template_logic/header.js
@@ -1,5 +1,5 @@
 Template.header.events({
-  'click .scroll-link': function(e){
+  'click .scroll-link': function(e) {
     e.preventDefault();
     var path = e.currentTarget.attributes['href'].value;
     var section = $("#" + path);
@@ -8,12 +8,37 @@ Template.header.events({
     Session.set('scrolling', true);
     $('html, body').animate({
       scrollTop: section.offset().top
-    }, 1000, function() { Session.set('scrolling', false);});
+    }, 1000, function() {
+      Session.set('scrolling', false);
+    });
   },
 
-  'click .navbar-brand': function(){
+  'click .navbar-brand': function() {
     $('html, body').animate({
       scrollTop: 0
     }, 500);
   }
+
+
+
+});
+
+var target = $('.section-title');
+var targetHeight = target.outerHeight();
+
+$(document).scroll(function(e){
+    //Navbar esta no topo
+    if(window.scrollY <= 130){
+        $('.navbar-default').css('background-color', 'transparent');
+        $('.navbar-default').css('margin-top', '3%');
+        $('.navbarLogo').css('width', 150);
+        
+    }
+    //Navbar esta acompanhando a rolagem
+    else {
+      $('.navbar-default').css('background-color', 'black');
+      $('.navbar-default').css('margin-top', '0');
+      $('.navbarLogo').css('width', 100);
+
+    }
 });
